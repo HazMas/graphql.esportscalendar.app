ac808ea3be60d1d7e47761f42bb09a5fc565ff0f
Santiago M.A <santiagomartinagra@gmail.com>
2018-07-01 23:15:33 +0200

    style(basic.spec): const en lugar de let

--- __test__/basic.spec.ts
+++ __test__/basic.spec.ts
@@ -1,7 +1,7 @@
 import { Basic } from '../src/basic'
 
 test('basic', () => {
-    let basic = new Basic()
+    const basic = new Basic()
     expect(basic.test()).toBe(0);
     expect(basic.test()).not.toBe(-10);
 });
