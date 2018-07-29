51346a64892968155ced877afc777856740c9e93
Santiago M.A <santiagomartinagra@gmail.com>
2018-07-29 22:41:08 +0200

    fix(jest.config): enviroment node

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,20 @@
 module.exports = {
-    "roots": [
-      "<rootDir>"
-    ],
-    "transform": {
-      "^.+\\.tsx?$": "ts-jest"
-    },
-    "reporters": ["default"],
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    "collectCoverageFrom": ["src/**/*.{ts,js}"],
-    "moduleFileExtensions": [
-      "ts",
-      "tsx",
-      "js",
-      "jsx",
-      "json",
-      "node"
-    ],
-  }
+  "testEnvironment": "node",
+  "roots": [
+    "<rootDir>"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "reporters": ["default"],
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "collectCoverageFrom": ["src/**/*.{ts,js}"],
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+}
